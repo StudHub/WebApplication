c138488c758fa07cbad339ce94114aa8cd216b07
MANISH PRAJAPATI <p.manish6897@gmail.com>
2017-09-17 21:59:59 +0530

    master page

--- WebApplication4/WebForm2.aspx.cs
+++ WebApplication4/WebForm2.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication4
 {
-    public partial class Site2 : System.Web.UI.MasterPage
+    public partial class WebForm2 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
